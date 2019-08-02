07311a3de194da4adcfa4e09dc7b307269ed7d5a
rosemarygutierrez17 <52755985+rosemarygutierrez17@users.noreply.github.com>
2019-08-01 19:49:10 -0700

    Add files via upload

---
+++ periodt.js
@@ -0,0 +1,44 @@
+
+
+function Image(){
+    if (document.getElementById("me2").src== "file:///Users/grosamar/Desktop/jan.jpg"){
+      document.getElementById("me2").src= "file:///Users/grosamar/Desktop/feb.jpg";
+    }
+    else if (document.getElementById("me2").src== "file:///Users/grosamar/Desktop/feb.jpg"){
+      document.getElementById("me2").src= "file:///Users/grosamar/Desktop/march.jpg";
+    }
+    else if (document.getElementById("me2").src== "file:///Users/grosamar/Desktop/march.jpg"){
+      document.getElementById("me2").src= "file:///Users/grosamar/Desktop/april.jpg";
+    }
+    else if (document.getElementById("me2").src==  "file:///Users/grosamar/Desktop/april.jpg"){
+      document.getElementById("me2").src= "file:///Users/grosamar/Desktop/may.jpg";
+    }
+    else if (document.getElementById("me2").src==  "file:///Users/grosamar/Desktop/may.jpg"){
+      document.getElementById("me2").src= "file:///Users/grosamar/Desktop/calendar.jpg";
+    }
+    else if (document.getElementById("me2").src==  "file:///Users/grosamar/Desktop/calendar.jpg"){
+      document.getElementById("me2").src= "file:///Users/grosamar/Desktop/july.png";
+    }
+    else if (document.getElementById("me2").src== "file:///Users/grosamar/Desktop/july.png"){
+      document.getElementById("me2").src= "file:///Users/grosamar/Desktop/aug.jpg";
+    }
+    else if (document.getElementById("me2").src== "file:///Users/grosamar/Desktop/aug.jpg"){
+      document.getElementById("me2").src= "file:///Users/grosamar/Desktop/sep.jpg";
+    }
+    else if (document.getElementById("me2").src==  "file:///Users/grosamar/Desktop/sep.jpg"){
+      document.getElementById("me2").src= "file:///Users/grosamar/Desktop/oct.jpg";
+    }
+    else if (document.getElementById("me2").src==  "file:///Users/grosamar/Desktop/oct.jpg"){
+      document.getElementById("me2").src= "file:///Users/grosamar/Desktop/nove.jpg";
+    }
+    else if (document.getElementById("me2").src==  "file:///Users/grosamar/Desktop/nove.jpgg"){
+      document.getElementById("me2").src= "file:///Users/grosamar/Desktop/decmber.jpg";
+    }
+    else if (document.getElementById("me2").src==  "file:///Users/grosamar/Desktop/decmber.jpg"){
+      document.getElementById("me2").src= "file:///Users/grosamar/Desktop/jan.jpg";
+    }
+    else{
+      document.getElementById("me2").src= "file:///Users/grosamar/Desktop/jan.jpg";
+    }
+//last one has to be an else
+}
